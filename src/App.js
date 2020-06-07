ed2193c4838002b98c1980cf1248570a82c6ffd7
RickyC0626 <rickychon99@gmail.com>
2020-06-07 13:29:40 -0400

    Create a router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 export default class App extends Component 
@@ -6,11 +7,13 @@ export default class App extends Component
     render()
     {
         return (
-            <div className="App">
-                <header className="App-header">
-                    
-                </header>
-            </div>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        
+                    </header>
+                </div>
+            </Router>
         );
     }
 }
