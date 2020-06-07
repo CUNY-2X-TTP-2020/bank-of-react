e1eb412582752b6a08d4dd11ae7a2ba0dd7a4d6b
RickyC0626 <rickychon99@gmail.com>
2020-06-07 15:51:32 -0400

    Add Login component, add mock login in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
+import Login from './components/Login';
 
 export default class App extends Component 
 {
@@ -21,11 +22,19 @@ export default class App extends Component
         }
     }
 
+    mockLogin = (loginInfo) =>
+    {
+        const newUser = {...this.state.currentUser};
+        newUser.userName = loginInfo.userName;
+        this.setState({ currentUser: newUser });
+    }
+
     render()
     {
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
         const UserProfileComponent = () => 
         (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />);
+        const LoginComponent = () => (<Login user={this.state.currentUser} mockLogin={this.mockLogin} {...this.props} />);
 
         return (
             <Router>
@@ -34,6 +43,7 @@ export default class App extends Component
                         <Switch>
                             <Route exact path="/" component={HomeComponent} />
                             <Route exact path="/userProfile" render={UserProfileComponent} />
+                            <Route exact path="/login" render={LoginComponent} />
                         </Switch>
                     </header>
                 </div>
