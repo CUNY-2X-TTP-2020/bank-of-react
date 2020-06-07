09fd7f02e268cfbb95b102d67b5526418de49552
RickyC0626 <rickychon99@gmail.com>
2020-06-07 14:59:14 -0400

    Add UserProfile component, create new route to it, create link to it in Home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 export default class App extends Component 
 {
@@ -11,13 +12,20 @@ export default class App extends Component
         super();
         this.state =
         {
-            accountBalance: 14568.27
+            accountBalance: 14568.27,
+            currentUser:
+            {
+                userName: 'bob_loblaw',
+                memberSince: '08/23/99',
+            }
         }
     }
 
     render()
     {
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
+        const UserProfileComponent = () => 
+        (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />);
 
         return (
             <Router>
@@ -25,6 +33,7 @@ export default class App extends Component
                     <header className="App-header">
                         <Switch>
                             <Route exact path="/" component={HomeComponent} />
+                            <Route exact path="/userProfile" render={UserProfileComponent} />
                         </Switch>
                     </header>
                 </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import AccountBalance from './AccountBalance';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component
 {
@@ -10,6 +11,8 @@ export default class Home extends Component
             <div>
                 <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/118/bank_1f3e6.png" alt="bank"/>
                 <h1>Bank of React</h1>
+
+                <Link to="/userProfile">User Profile</Link>
 
                 <AccountBalance accountBalance={this.props.accountBalance} />
             </div>
