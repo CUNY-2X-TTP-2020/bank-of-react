c94396a55938942dcdd1554b9a2f779efb22efc8
RickyC0626 <rickychon99@gmail.com>
2020-06-08 01:49:05 -0400

    Separate styling for centered elements, add single Login button on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import AccountBalance from './AccountBalance';
-
 /**
  * This component will be the homepage of the app, which will show the app's logo and name, along with
  * a login button that redirects users to the Login page
@@ -12,12 +10,13 @@ export default class Home extends Component
     render()
     {
         return (
-            <div>
+            <div className="page-header">
                 <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/118/bank_1f3e6.png" alt="bank"/>
                 <h1>Bank of React</h1>
 
-                <Link to="/debits">Debits</Link><br/>
-                <Link to="/credits">Credits</Link>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
             </div>
         );
     }
