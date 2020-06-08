23d2b6186537b8adbd2fa0319173f26d6b84cea2
RickyC0626 <rickychon99@gmail.com>
2020-06-08 02:54:23 -0400

    Add links in UserProfile to different pages, revamp design

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -11,13 +11,21 @@ export default class UserProfile extends Component
     render()
     {
         return (
-            <div>
-                <h1>User Profile</h1>
+            <div className="page-header">
+                <fieldset>
+                    <legend>User Profile</legend>
 
-                <div>UserName: {this.props.username}</div>
-                <div>Member Since: {this.props.memberSince}</div>
+                    <div>Username: {this.props.username}</div>
+                    <div>Member Since: {this.props.memberSince}</div><br/>
 
-                <Link to="/">Home</Link>
+                    <fieldset>
+                        <legend>Options</legend>
+
+                        <Link to="/debits"><button>View Debits</button></Link>
+                        <Link to="/credits"><button>View Credits</button></Link>
+                        <Link to="/"><button>Logout</button></Link>
+                    </fieldset>
+                </fieldset>
             </div>
         );
     }
