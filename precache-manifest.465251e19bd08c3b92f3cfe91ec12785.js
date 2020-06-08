e1e5c5ff2c4b1fdd04142f87c20c8f72254c7800
RickyC0626 <rickychon99@gmail.com>
2020-06-08 00:49:57 -0400

    Updates

---
+++ precache-manifest.465251e19bd08c3b92f3cfe91ec12785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dec46eaa5d4423aa2b25151450bd23",
+    "url": "/bank-of-react/index.html"
+  },
+  {
+    "revision": "405851ea67b9d4192833",
+    "url": "/bank-of-react/static/css/main.ab3d23ba.chunk.css"
+  },
+  {
+    "revision": "de59392b9c6b9dbfb88e",
+    "url": "/bank-of-react/static/js/2.4950bdd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank-of-react/static/js/2.4950bdd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405851ea67b9d4192833",
+    "url": "/bank-of-react/static/js/main.b3c84911.chunk.js"
+  },
+  {
+    "revision": "4d69809c6a13bffbdf90",
+    "url": "/bank-of-react/static/js/runtime-main.36cf4d5d.js"
+  }
+]);
